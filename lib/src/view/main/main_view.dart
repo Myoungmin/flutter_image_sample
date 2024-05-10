c33261dd42698d95b4d5a03b427dc293ccd651cb
AzureAD\고명민 <miung222@gmail.com>
2024-05-10 23:32:13 +0900

    MouseRegion으로 마우스 위치 좌표 표시

--- lib/src/view/main/main_view.dart
+++ lib/src/view/main/main_view.dart
@@ -4,8 +4,15 @@ import 'package:flutter_image_sample/src/view/base_view.dart';
 import 'package:flutter_image_sample/src/view/main/main_view_model.dart';
 import 'package:flutter_image_sample/theme/component/drawing_page.dart';
 
-class MainView extends StatelessWidget {
-  const MainView({super.key});
+class MainView extends StatefulWidget {
+  const MainView({Key? key}) : super(key: key);
+
+  @override
+  _MainViewState createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  String _position = 'Mouse Position: ';
 
   @override
   Widget build(BuildContext context) {
@@ -20,17 +27,29 @@ class MainView extends StatelessWidget {
             ),
           ),
         ),
-        body: const Row(
+        body: Row(
           children: [
             Expanded(
               child: Center(
-                child: Image(
-                  image: NetworkImage(
-                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
+                child: MouseRegion(
+                  onHover: (PointerEvent event) {
+                    setState(() {
+                      _position =
+                          'Mouse Position\nx=${event.localPosition.dx.toInt()}\ny=${event.localPosition.dy.toInt()}';
+                    });
+                  },
+                  child: Image.network(
+                    'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                  ),
                 ),
               ),
             ),
             Expanded(
+              child: Center(
+                child: Text(_position),
+              ),
+            ),
+            const Expanded(
               child: Center(
                 child: DrawingPage(),
               ),
