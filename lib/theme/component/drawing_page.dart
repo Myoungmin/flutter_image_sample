e29a44137a262fab569e41876a131d00c50dea0b
AzureAD\고명민 <miung222@gmail.com>
2024-05-12 00:52:42 +0900

    CheckboxListTile을 추가하여 Annotation 추가, 그리기 여부 설정

--- lib/theme/component/drawing_page.dart
+++ lib/theme/component/drawing_page.dart
@@ -35,23 +35,31 @@ class _DrawingPageState extends State<DrawingPage> {
         ..style = PaintingStyle.stroke;
       switch (controller.currentMode) {
         case DrawingMode.point:
-          paint.color = Colors.red;
-          controller
-              .addAnnotation(PointAnnotation(controller.endingPoint, paint));
+          if (controller.showPoint) {
+            paint.color = Colors.red;
+            controller
+                .addAnnotation(PointAnnotation(controller.endingPoint, paint));
+          }
           break;
         case DrawingMode.line:
-          paint.color = Colors.green;
-          controller.addAnnotation(LineAnnotation(
-              controller.startingPoint, controller.endingPoint, paint));
+          if (controller.showLine) {
+            paint.color = Colors.green;
+            controller.addAnnotation(LineAnnotation(
+                controller.startingPoint, controller.endingPoint, paint));
+          }
           break;
         case DrawingMode.rectangle:
-          paint.color = Colors.blue;
-          controller.addAnnotation(RectAnnotation(
-              controller.startingPoint, controller.endingPoint, paint));
+          if (controller.showRect) {
+            paint.color = Colors.blue;
+            controller.addAnnotation(RectAnnotation(
+                controller.startingPoint, controller.endingPoint, paint));
+          }
           break;
         case DrawingMode.text:
-          controller.addAnnotation(
-              TextAnnotation("Sampe Text", controller.endingPoint, paint));
+          if (controller.showText) {
+            controller.addAnnotation(
+                TextAnnotation("Sample Text", controller.endingPoint, paint));
+          }
           break;
       }
     });
@@ -85,6 +93,46 @@ class _DrawingPageState extends State<DrawingPage> {
                 ),
               ),
             ),
+          ),
+          Wrap(
+            children: [
+              CheckboxListTile(
+                title: const Text("Show Points"),
+                value: controller.showPoint,
+                onChanged: (bool? value) {
+                  setState(() {
+                    controller.showPoint = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: const Text("Show Lines"),
+                value: controller.showLine,
+                onChanged: (bool? value) {
+                  setState(() {
+                    controller.showLine = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: const Text("Show Rectangles"),
+                value: controller.showRect,
+                onChanged: (bool? value) {
+                  setState(() {
+                    controller.showRect = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: const Text("Show Text"),
+                value: controller.showText,
+                onChanged: (bool? value) {
+                  setState(() {
+                    controller.showText = value!;
+                  });
+                },
+              ),
+            ],
           ),
           ButtonBar(
             alignment: MainAxisAlignment.center,
@@ -171,9 +219,12 @@ class AnnotationController {
   String position = 'Mouse Position: ';
   DrawingMode currentMode = DrawingMode.point;
   List<Annotation> annotations = [];
+  bool showPoint = true;
+  bool showLine = true;
+  bool showRect = true;
+  bool showText = true;
 
-  void addAnnotation(Annotation annoation) => annotations.add(annoation);
-
+  void addAnnotation(Annotation annotation) => annotations.add(annotation);
   void clear() => annotations.clear();
 }
 
@@ -185,7 +236,12 @@ class AnnotationPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     for (var element in controller.annotations) {
-      element.draw(canvas);
+      if ((element is PointAnnotation && controller.showPoint) ||
+          (element is LineAnnotation && controller.showLine) ||
+          (element is RectAnnotation && controller.showRect) ||
+          (element is TextAnnotation && controller.showText)) {
+        element.draw(canvas);
+      }
     }
   }
 
