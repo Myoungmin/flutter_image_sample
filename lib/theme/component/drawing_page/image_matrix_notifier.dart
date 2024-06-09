160192d9788d535f48c6a1e2bc8e41c2d9d7549b
Myoungmin <miung222@gmail.com>
2024-06-09 12:30:17 +0900

    translateByOffset, scaleByFactor 추가

--- lib/theme/component/drawing_page/image_matrix_notifier.dart
+++ lib/theme/component/drawing_page/image_matrix_notifier.dart
@@ -13,4 +13,16 @@ class ImageMatrixNotifier extends Notifier<Matrix4> {
     final rotation = Matrix4.identity()..rotateZ(90 * 3.1415927 / 180);
     state = currentMatrix.multiplied(rotation);
   }
+
+  void translateByOffset(Offset offset) {
+    final currentMatrix = state.clone();
+    final translation = Matrix4.identity()..translate(offset.dx, offset.dy);
+    state = currentMatrix.multiplied(translation);
+  }
+
+  void scaleByFactor(double scaleFactor) {
+    final currentMatrix = state.clone();
+    final scaling = Matrix4.identity()..scale(scaleFactor, scaleFactor);
+    state = currentMatrix.multiplied(scaling);
+  }
 }
